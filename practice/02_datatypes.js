5f214c4098ee71b61b75ca9166f07cc89daf8cc8
Divyanshi <divyanshisagar1742007@gmail.com>
2026-01-04 23:40:36 +0530

    dataTypes

---
+++ practice/02_datatypes.js
@@ -0,0 +1,19 @@
+/* 
+    number => 2 to power 53 -1
+    bigint => larger than number
+    string => text
+    boolean => true or false    
+    null => empty value/ it is an object in js
+    undefined => value is not assigned
+    object => collection of properties
+*/
+
+//symbol => unique identifier
+
+
+let age=18;
+let name="Divyanshi"
+let isLoggedIn=false;
+
+
+console.log(typeof null);
