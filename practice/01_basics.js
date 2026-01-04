be98b5654a6db33020c84532c10dcf99df495511
Divyanshi <divyanshisagar1742007@gmail.com>
2026-01-04 23:23:30 +0530

    basics of js

---
+++ practice/01_basics.js
@@ -0,0 +1,10 @@
+const accountid=123454;
+let accountmail= "abc@gmail.com";
+var accountpassword= "heloooo13468";
+accountCity ="Dehradun";
+
+/* prefer not to use var it can create some issue in block scope*/
+
+
+console.log(accountid);
+console.table([accountid,accountmail,accountpassword,accountCity]);
