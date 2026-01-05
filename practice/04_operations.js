9f45b301c07cd4c5f6fe6b8f1fd3691993fab6cd
Divyanshi <divyanshisagar1742007@gmail.com>
2026-01-05 23:26:02 +0530

    operations

---
+++ practice/04_operations.js
@@ -0,0 +1,11 @@
+let value=4
+let negvalue= -value;
+console.log(negvalue);
+console.log(typeof negvalue);
+
+
+str1="Hello"
+str2=" World"    
+str3= str1+str2;
+console.log(str3);
+console.log(typeof str3);
